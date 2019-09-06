f0938fa2ad82e840f6532f8dc7aa70d5f77118fb
Anson Kwok <ansonkwok0@gmail.com>
2019-09-06 15:57:18 +1000

    Update syd_flights.c

--- syd_flights.c
+++ syd_flights.c
@@ -27,6 +27,7 @@
 #define MAX_CITYCODE_LEN 3
 #define MAX_NUM_FLIGHTS 5
 #define DB_NAME "database"
+#define ORIGIN_NAME "SYD"
 
 /*******************************************************************************
  * List structs - you may define struct date_time and struct flight only. Each
@@ -59,6 +60,14 @@ int isValidFlightCode(char code[]);
 int isValidDt(date_time_t dt);
 int isValidArrivalCode(char code[]);
 void displayFlights(flight_t flights[], int nFlights);
+void printAllFlights(flight_t flights[], int nFlights);
+void printFlights(flight_t flights[], int nFlights, char input[]);
+void printFlight(flight_t flight);
+void printHeader(void);
+void printSpace(char code[], int length);
+void printFlightDt(date_time_t dt);
+void saveFlights(flight_t flights[], int nFlights);
+int loadFlights(flight_t flights[]);
 
 /*******************************************************************************
  * Main
@@ -66,7 +75,7 @@ void displayFlights(flight_t flights[], int nFlights);
 int main(void)
 {
     /* TODO */
-    int userInput;
+    int userInput = 0;
     flight_t flights[MAX_NUM_FLIGHTS];
     int numOfFlights = 0;
     while (userInput != 5) {
@@ -81,7 +90,12 @@ int main(void)
             break;
             case 2: displayFlights(flights, numOfFlights);
             break;
-    
+            case 3: saveFlights(flights, numOfFlights);
+            break;
+            case 4: numOfFlights = loadFlights(flights);
+            break;
+            case 5:
+            break;
             default:
             break;
         }
@@ -126,8 +140,8 @@ void addFlight(flight_t flights[], int nFlights) {
             if (!flightCodeIsValid) printf("Invalid input\n");
         }
         
+        printf("Enter departure info for the flight leaving SYD.\n");
         while (!departureDtIsValid) {
-            printf("Enter departure info for the flight leaving SYD.\n");
             printf("Enter month, date, hour and minute separated by spaces>\n");
             scanf("%d %d %d %d", &newFlight.departure_dt.month, &newFlight.departure_dt.day, &newFlight.departure_dt.hour, &newFlight.departure_dt.minute);
 
@@ -143,8 +157,8 @@ void addFlight(flight_t flights[], int nFlights) {
             if (!arrivalCodeIsValid) printf("Invalid input\n");
         }
         
+        printf("Enter arrival info.\n");
         while (!arrivalDtIsValid) {
-            printf("Enter arrival info.\n");
             printf("Enter month, date, hour and minute separated by spaces>\n");
             scanf("%d %d %d %d", &newFlight.arrival_dt.month, &newFlight.arrival_dt.day, &newFlight.arrival_dt.hour, &newFlight.arrival_dt.minute);
 
@@ -158,7 +172,16 @@ void addFlight(flight_t flights[], int nFlights) {
 }
 
 int isValidFlightCode(char code[]) {
-    return strlen(code) < MAX_FLIGHTCODE_LEN + 1;
+    if (strlen(code) > MAX_FLIGHTCODE_LEN || strlen(code) < 3) return 0;
+    else if (code[0] < 'A' || code[0] > 'Z') return 0;
+    else if (code[1] < 'A' || code[1] > 'Z') return 0;
+    else {
+        int codeIndex;
+        for (codeIndex = 2; codeIndex < strlen(code); codeIndex++) {
+            if ((int)code[codeIndex] < '0' || (int)code[codeIndex] > '9') return 0;
+        }
+    }
+    return 1;
 }
 
 int isValidDt(date_time_t dt) {
@@ -173,18 +196,107 @@ int isValidArrivalCode(char code[]) {
 }
 
 void displayFlights(flight_t flights[], int nFlights) {
-    int index;
+    char input[MAX_CITYCODE_LEN + 1];
+    int arrivalCodeIsValid = 0;
 
-    printf("Enter arrival city code or enter * to show all destinations>\n");
-    for (index = 0; index < nFlights; index++) {
-        flight_t eachFlight = flights[index];
-        printf("%s SYD %02d-%02d %02d:%02d %s %02d-%02d %02d:%02d", eachFlight.flightcode, 
-                                                                    eachFlight.departure_dt.month, eachFlight.departure_dt.day, 
-                                                                    eachFlight.departure_dt.hour, eachFlight.departure_dt.minute, 
-                                                                    eachFlight.arrival_city,
-                                                                    eachFlight.arrival_dt.month, eachFlight.arrival_dt.day,
-                                                                    eachFlight.arrival_dt.hour, eachFlight.arrival_dt.minute);
+    while (!arrivalCodeIsValid) {
+        printf("Enter arrival city code or enter * to show all destinations>\n");
+        scanf("%s", input);
+
+        arrivalCodeIsValid = isValidArrivalCode(input);
+        if (arrivalCodeIsValid) {
+            if (nFlights == 0) printf("No flights\n");
+            else if (strcmp(input, "*") == 0) {
+                printAllFlights(flights, nFlights);
+            } else printFlights(flights, nFlights, input);
+        } else printf("Invalid input\n");
     }
+}
 
-    /* if none: "No flights" */
+void printAllFlights(flight_t flights[], int nFlights) {
+    int index = 0;
+    
+    if (nFlights > 0) {
+        printHeader();
+        for (index = 0; index < nFlights; index++) {
+            flight_t eachFlight = flights[index];
+            printFlight(eachFlight);
+        }
+    }
+}
+
+void printFlights(flight_t flights[], int nFlights, char input[]) {
+    int index, isFound;
+    index = 0;
+    isFound = 0;
+    if (nFlights > 0) {
+        for (index = 0; index < nFlights; index++) {
+            flight_t thisFlight = flights[index];
+            if (strcmp(thisFlight.arrival_city, input) == 0) {
+                if (!isFound) {
+                    printHeader();
+                    isFound = 1;
+                }
+                printFlight(thisFlight);
+            }
+        }
+        if (!isFound) printf("No flights\n");
+    } else printf("No flights\n");
+}
+
+void printFlight(flight_t flight) {
+    printf("%s ", flight.flightcode);
+    printSpace(flight.flightcode, MAX_FLIGHTCODE_LEN);
+
+    printf("%s ",ORIGIN_NAME);
+    printFlightDt(flight.departure_dt);
+
+    printf(" %s ", flight.arrival_city);
+    printSpace(flight.arrival_city, MAX_CITYCODE_LEN);
+
+    printFlightDt(flight.arrival_dt);
+
+    printf("\n");
+}
+
+void printSpace(char code[], int length) {
+    int spaceLength = length - strlen(code);
+    int index;
+    for (index = 0; index < spaceLength; index++) {
+        printf(" ");
+    }
+}
+
+void printFlightDt(date_time_t dt) {
+    printf("%02d-%02d %02d:%02d", dt.month, dt.day, dt.hour, dt.minute);
+}
+
+void printHeader(void) {
+    printf("Flight Origin          Destination\n");
+    printf("------ --------------- ---------------\n");
+}
+
+void saveFlights(flight_t flights[], int nFlights) {
+    FILE *saveFile = fopen(DB_NAME, "w");
+
+    int index = 0;
+    for (index = 0; index < nFlights; index++) {
+        fwrite(&flights[index], sizeof(flight_t), 1, saveFile);
+    }
+    fclose(saveFile);
+}
+
+int loadFlights(flight_t flights[]) {
+    FILE *loadFile = fopen(DB_NAME, "rb");
+
+    int index = 0;
+    if (loadFile) {
+        while(!feof(loadFile)) {
+            fread(&flights[index], sizeof(flight_t), 1, loadFile);
+            index ++;
+        }
+        fclose(loadFile);
+    } else printf("Read error\n");
+
+    return index - 1;
 }
